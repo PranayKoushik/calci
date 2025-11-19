b798a87a299c8109fad2980e2d90f186b7a1630c
sai kumar reddy <mudireddysaikumarreddy9225@gmail.com>
2025-11-19 18:50:32 +0530

    power upload

--- src/com/calci/CalciImplementation.java
+++ src/com/calci/CalciImplementation.java
@@ -7,7 +7,6 @@ public class CalciImplementation implements Calci {
 		int c=a+b;
 		return c;
 	}
-
 	@Override
 	public int sub(int a, int b) {
 	    int c=a-b;
@@ -24,6 +23,15 @@ public class CalciImplementation implements Calci {
 	public int div(int a, int b) {
 		int c=a/b;
 		return c;
+	}
+	
+	public int power(int a,int b) {
+	     int result =1;
+	     for(int i=0;i<b;i++) {
+	    	 result =result*a;
+	    }
+	     return result;
+		
 	}
 
 }
